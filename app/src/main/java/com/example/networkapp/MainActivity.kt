460c150fd7e1f87eb326e388b9534a50e3cef6d2
jlawall9 <johnlawall24@gmail.com>
2024-11-19 12:25:17 -0500

    Works

--- app/src/main/java/com/example/networkapp/MainActivity.kt
+++ app/src/main/java/com/example/networkapp/MainActivity.kt
@@ -27,6 +27,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var numberEditText: EditText
     lateinit var showButton: Button
     lateinit var comicImageView: ImageView
+    private var searchHistory: MutableList<Int> = mutableListOf()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +41,7 @@ class MainActivity : AppCompatActivity() {
         numberEditText = findViewById<EditText>(R.id.comicNumberEditText)
         showButton = findViewById<Button>(R.id.showComicButton)
         comicImageView = findViewById<ImageView>(R.id.comicImageView)
+loadSavedComic()
 
         showButton.setOnClickListener {
             downloadComic(numberEditText.text.toString())
@@ -51,8 +54,12 @@ class MainActivity : AppCompatActivity() {
         val url = "https://xkcd.com/$comicId/info.0.json"
         requestQueue.add (
             JsonObjectRequest(url
-                , {showComic(it)}
-                , {}
+                ,{image ->
+                    showComic(image)
+                    saveComic(image)}
+
+
+                ,{}
             )
         )
     }
@@ -66,7 +73,28 @@ class MainActivity : AppCompatActivity() {
 
     // Implement this function
     private fun saveComic(comicObject: JSONObject) {
+        val sharedPreferences = getSharedPreferences("comics", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("title", comicObject.getString("title"))
+        editor.putString("description", comicObject.getString("alt"))
+        editor.putString("imageUrl", comicObject.getString("img"))
+        editor.apply()
 
+    }
+
+    fun loadSavedComic(){
+        var sharedPreferences = getSharedPreferences("comics", MODE_PRIVATE)
+        val title = sharedPreferences.getString("title", "")
+        val description = sharedPreferences.getString("description", "")
+        val imageUrl = sharedPreferences.getString("imageUrl", "")
+
+        if (!title.isNullOrEmpty() && !description.isNullOrEmpty() && !imageUrl.isNullOrEmpty()){
+            titleTextView.text = title
+            descriptionTextView.text = description
+            Picasso.get().load(imageUrl).into(comicImageView)
+        } else{
+            Toast.makeText(this,"No saved comic found", Toast.LENGTH_SHORT).show()
+        }
     }
 
 
